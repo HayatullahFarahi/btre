ff84ec7bc244deaec89f0375534ff43cbc4e7bbc
HayatullahFarahi <hayatullah.farahi@gmail.com>
2021-06-28 16:11:17 +0430

    authentication

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,7 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+//alert message fade out
+setTimeout(function () {
+  $('#message').fadeOut('slow');
+}, 3000);
